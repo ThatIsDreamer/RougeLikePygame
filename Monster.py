dab3160c0e366e2f93254f85cba05d57eeb4d1ec
JOJARvevo <150832156+JOJARvevo@users.noreply.github.com>
2023-12-13 22:55:20 +0300

    Add files via upload

---
+++ Monster.py
@@ -0,0 +1,77 @@
+import math
+
+import pygame
+import Player
+
+def get_image(sheet, width, hieght, x, y):
+    # pygame.SRCALPHA
+    image = pygame.Surface((width, hieght), pygame.SRCALPHA)
+    image.blit(sheet, (0, 0), (x, y, width, hieght))
+    image = pygame.transform.scale(image, (128, 128))
+
+    return image
+
+class Monster(pygame.sprite.Sprite):
+    def __init__(self, pos, group, walls):
+        super().__init__(group)
+        all_anims = ["skeleton_v2_1.png", "skeleton_v2_2.png",  "skeleton_v2_3.png", "skeleton_v2_4.png"]
+        self.animations = [[], []]
+        for i in range(4):
+            self.animations[1].append(get_image(pygame.image.load(f'Assets/Characters/{all_anims[i]}'), 16, 16, 0, 0))
+        for i in range(4):
+            im = pygame.image.load(f'Assets/Characters/{all_anims[i]}')
+            im = pygame.transform.flip(im, True, False)
+            self.animations[0].append(get_image(im, 16, 16, 0, 0))
+        self.image = self.animations[0][0]
+        self.rect = self.image.get_rect(center=pos)
+        self.direction = pygame.math.Vector2()
+        self.currsprite = 0
+        self.speed = 0.9
+        self.curranimation = 0
+        self.walls = walls
+        self.p_x = -0
+        self.p_y = -0
+
+    def move(self, dx=0, dy=0):
+        if dx != 0:
+            self.rect.x += dx
+            if pygame.sprite.spritecollide(self, self.walls, False):
+                if dx > 0:
+                    self.rect.right = min(
+                        wall.rect.left for wall in pygame.sprite.spritecollide(self, self.walls, False))
+                elif dx < 0:
+                    self.rect.left = max(
+                        wall.rect.right for wall in pygame.sprite.spritecollide(self, self.walls, False))
+
+        if dy != 0:
+            self.rect.y += dy
+            if pygame.sprite.spritecollide(self, self.walls, False):
+                if dy > 0:
+                    self.rect.bottom = min(
+                        wall.rect.top for wall in pygame.sprite.spritecollide(self, self.walls, False))
+                elif dy < 0:
+                    self.rect.top = max(
+                        wall.rect.bottom for wall in pygame.sprite.spritecollide(self, self.walls, False))
+
+    def player_move(self, player_x, player_y):
+        self.p_x  = player_x
+        self.p_y = player_y
+
+    def update(self):
+        temp_x = self.p_x - self.rect.x
+        temp_y = self.p_y - self.rect.y
+        distance = math.sqrt(temp_x ** 2 + temp_y ** 2)
+        if distance > 0:
+            temp_x, temp_y = temp_x / distance, temp_y / distance
+        self.move(temp_x * self.speed, temp_y * self.speed)
+
+        if self.p_x > self.rect.x:
+            self.currsprite = 1
+        else:
+            self.currsprite = 0
+
+        self.curranimation += 0.01
+        if self.curranimation > 3:
+            self.curranimation = 0
+
+        self.image = self.animations[self.currsprite][int(self.curranimation)]
